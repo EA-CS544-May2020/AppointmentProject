d63019b1aae352ea63c1ada587f20c2727bc83cf
kevin nguyen <ndkhanh99.job@gmail.com>
2020-05-11 15:59:45 -0600

    Revert "Init project source code"
    This reverts commit abce5edd19fbb7debcdbb327fd10fce832546606.

--- src/main/java/cs544/project/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package cs544.project.service;
-
-import cs544.project.domain.User;
-import cs544.project.service.response.UserResponse;
-import edu.miu.common.service.BaseReadService;
-
-public interface IUserService extends BaseReadService<UserResponse, User, Integer> {
-
-}

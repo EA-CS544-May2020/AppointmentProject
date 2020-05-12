d7b7279b8d6571e7976e53a99ec4398552903d67
jpalmat <jimmy.palma1990@gmail.com>
2020-05-12 15:04:33 -0500

    transactional anotation

--- src/main/java/cs544/project/service/impl/ReservationServiceImpl.java
+++ src/main/java/cs544/project/service/impl/ReservationServiceImpl.java
@@ -1,6 +1,7 @@
 package cs544.project.service.impl;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import cs544.project.domain.Reservation;
 import cs544.project.service.IReservationService;
@@ -8,6 +9,7 @@ import cs544.project.service.response.ReservationResponse;
 import edu.miu.common.service.BaseReadServiceImpl;
 
 @Service
+@Transactional
 public class ReservationServiceImpl extends BaseReadServiceImpl<ReservationResponse, Reservation, Integer> implements IReservationService{
 
 }
